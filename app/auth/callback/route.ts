6c9ecd93f313d1e47006731d938f2a3bc3dbdb4a
Olenaideole <146712702+Olenaideole@users.noreply.github.com>
2025-05-23 19:47:28 +0300

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -14,5 +14,6 @@ export async function GET(request: NextRequest) {
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin + "/dashboard")
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || requestUrl.origin
+return NextResponse.redirect(baseUrl + "/dashboard")
 }
